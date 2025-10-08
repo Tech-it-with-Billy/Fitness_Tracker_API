6ad57300350b41fde4226b32da5ebba8174fd9b8
Billy Och <ochiengbilly02@gmail.com>
2025-10-08 15:40:15 +0300

    Update: Adding  the User and Activity apps to handle user registration, login, authentication and manage fitness activities CRUD

---
+++ Activity/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.IntegerField(help_text="Duration in minutes")
+    calories_burned = models.IntegerField()
+
+    def __str__(self):
+        return self.name
---
+++ User/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
