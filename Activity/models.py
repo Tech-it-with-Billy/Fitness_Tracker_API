a6d0021a9e2bde0717a816f94e20027b76efed8f
Billy Och <ochiengbilly02@gmail.com>
2025-10-22 00:05:10 +0300

    Update: Connecting the Activity model to the User app and adding activity fields

--- Activity/models.py
+++ Activity/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Activity(models.Model):
-    name = models.CharField(max_length=100)
-    duration = models.IntegerField(help_text="Duration in minutes")
-    calories_burned = models.IntegerField()
-
-    def __str__(self):
-        return self.name
+    ACTIVITY_CHOICE = [
+        ('Running', 'Running'),
+        ('Cycling', 'Cycling'),
+        ('Swimming', 'Swimming'),
+        ('Walking', 'Walking'),
+        ('Gym', 'Gym'),
+        ('Other', 'Other'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    activity_type = models.CharField(max_length= 50, choices=ACTIVITY_CHOICE)
+    title = models.CharField(max_length=100)
+    duration = models.DurationField(help_text='Activity duration')
+    distance = models.FloatField(null=True, blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     
+    def __str__(self):
+        return f'{self.activity_type} - {self.title} ({self.user.username})'
     
